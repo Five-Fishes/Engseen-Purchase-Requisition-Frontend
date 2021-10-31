feb485d99dfe5bfe6de9c3511762c50f0d0d4c7a
HowardYaw <yjhhoward@gmail.com>
2021-10-31 17:52:08 +0800

    purchase requisition template basic table stucture

---
+++ src/pages/purchase-requisition-template/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RouteComponentProps, Route, Switch } from 'react-router-dom';
+import { PurchaseRequisitionTemplateList } from './purchase-requisition-template-list';
+
+const Routes: React.FC<RouteComponentProps> = ({ match }) => (
+  <>
+    <Switch>
+      <Route path={match.url} component={PurchaseRequisitionTemplateList} />
+    </Switch>
+  </>
+);
+
+export default Routes;

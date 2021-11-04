159d04fb2469d7ac89fb814b4ec9ea7d10c6de31
Lu Xianze <luxianze98@gmail.com>
2021-11-04 13:30:34 +0800

    Revert mock log in

--- src/modules/shared/components/PostLogInRoute.tsx
+++ src/modules/shared/components/PostLogInRoute.tsx
@@ -1,17 +1,9 @@
-import { useEffect, useState } from "react";
 import { Redirect, Route, RouteProps } from "react-router-dom";
 
 interface IPostLogInRouteProps extends RouteProps {}
 
 const PostLogInRoute: React.FC<IPostLogInRouteProps> = (props) => {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    setInterval(() => {
-      const loggeInStatus = Boolean(localStorage.getItem("loggedIn"));
-      setLoggedIn(loggeInStatus);
-    }, 500);
-  }, [loggedIn]);
+  const loggedIn = true;
 
   return (
     <>

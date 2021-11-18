730fcc9cadf2a5aff7126fa3081b5fc7854c3dd7
HowardYaw <yjhhoward@gmail.com>
2021-11-18 23:49:55 +0800

    exception handle for date time format

--- src/utils/date-time/date-time-format.ts
+++ src/utils/date-time/date-time-format.ts
@@ -2,11 +2,15 @@ export const convertToLocalString: any = (dateTime?: any) => {
   if (dateTime === undefined) {
     return "";
   }
-  const dateTimeValue = new Date(dateTime);
-  if (!(dateTimeValue instanceof Date) || dateTimeValue.toString() === "Invalid Date") {
+  try {
+    const dateTimeValue = new Date(dateTime);
+    if (!(dateTimeValue instanceof Date) || dateTimeValue.toString() === "Invalid Date") {
+      return "";
+    }
+    const localeDate = dateTimeValue.getDate() + "/" + Number(dateTimeValue.getMonth() + 1) + "/" + dateTimeValue.getFullYear();
+    const localeTime = dateTimeValue.toLocaleTimeString();
+    return localeDate + " " + localeTime;
+  } catch (error) {
     return "";
   }
-  const localeDate = dateTimeValue.getDate() + "/" + Number(dateTimeValue.getMonth() + 1) + "/" + dateTimeValue.getFullYear();
-  const localeTime = dateTimeValue.toLocaleTimeString();
-  return localeDate + " " + localeTime;
 }

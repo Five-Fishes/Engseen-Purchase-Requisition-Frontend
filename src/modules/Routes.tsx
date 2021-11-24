fe168a7fd898ee75a6510b12f291acbf95c811f8
LUXIANZE <luxianze98@gmail.com>
2021-11-24 23:29:30 +0800

    Enable routing to always have fallback

--- src/modules/Routes.tsx
+++ src/modules/Routes.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
-import NoMatch from "./shared/components/NoMatch";
 import PageRefresh from "./shared/components/RefreshRoute";
 import PostLogInRoute from "./shared/components/PostLogInRoute";
+import SwitchWithFallback from "./shared/components/SwitchWithFallback";
 
 import LoginPage from "./login/pages/login";
 import PurchaseRequisitionTemplateRoute from "./purchase-requisition-template/pages";
@@ -14,7 +14,7 @@ import PurchaseRequisitionSubmissionPage from "./purchase-requisition-submissioi
 const Routes: React.FC = () => {
   
   return (
-    <Switch>
+    <SwitchWithFallback>
       <Route exact path="/" component={LoginPage} />
       <Route path="/page-refresh/:destination" component={PageRefresh} />
       <PostLogInRoute path="/purchase-requisition-template" component={PurchaseRequisitionTemplateRoute} />
@@ -22,10 +22,7 @@ const Routes: React.FC = () => {
       <PostLogInRoute path="/purchase-requisition-submission-record" component={PurchaseRequisitionSubmissionPage}/>
       <PostLogInRoute path="/purchase-requisition-approval" component={PurchaseRequisitionApprovalPage}/>
       {/* <PostLogInRoute path="/purchase-order" component={PurchaseRequisitionRequestPage}/>  */}
-      <Route path="*">
-        <NoMatch />
-      </Route>
-    </Switch>
+    </SwitchWithFallback>
   );
 };
 

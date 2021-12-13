085dd68575862bb331d8f1be90fa81aaeca42a76
LUXIANZE <luxianze98@gmail.com>
2021-12-13 23:30:43 +0800

    Show 100 per page by default

--- src/constant/pagination-config.ts
+++ src/constant/pagination-config.ts
@@ -1,3 +1,3 @@
 import { TablePaginationConfig } from "antd";
 
-export const TABLE_PAGINATION_CONFIG: TablePaginationConfig = { pageSizeOptions: ["5", "10", "20", "50", "100"], hideOnSinglePage: false, defaultPageSize: 5, showSizeChanger: true };
+export const TABLE_PAGINATION_CONFIG: TablePaginationConfig = { pageSizeOptions: ["5", "10", "20", "50", "100"], hideOnSinglePage: false, defaultPageSize: 100, showSizeChanger: true };

a41dbb14a2d9e62b9d50f29da6db8ae38369dbc7
LUXIANZE <luxianze98@gmail.com>
2022-06-17 23:01:48 +0800

    Done btn navigation to POR records page

--- src/modules/purchase-order-receipt-creation/pages/purchase-order-receipt-creation-page.tsx
+++ src/modules/purchase-order-receipt-creation/pages/purchase-order-receipt-creation-page.tsx
@@ -25,7 +25,7 @@ import { Button, Drawer, Input, Popover, Table } from 'antd';
 import Title from 'antd/lib/typography/Title';
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps, useHistory } from 'react-router-dom';
 import PurchaseOrderReceiptHeaderInfo from '../components/receipt-header-info/receipt-header-info';
 import PurchaseOrderReceiptCreationRequestConstructor from '../components/request-constructor/request-constructor';
 import generateIndex from '../components/request-constructor/request-constructor-indexer';
@@ -35,6 +35,7 @@ import PurchaseOrderReceiptCreationTableDisplaySettings from '../components/tabl
 interface IPurchaseOrderReceiptCreationPageProps extends StateProps, DispatchProps, RouteComponentProps<{ vendorId: string }> {}
 
 const PurchaseOrderReceiptCreationPage: React.FC<IPurchaseOrderReceiptCreationPageProps> = (props: IPurchaseOrderReceiptCreationPageProps) => {
+  const history = useHistory();
   const [submissionInProgress, setSubmissionInProgress] = useState<boolean>(false);
   const [purchaseOrderItem, setPurchaseOrderItem] = useState<IPurchaseOrderItem[]>();
   const [searchResult, setSearchResult] = useState<IPurchaseOrderItem[]>();
@@ -176,6 +177,7 @@ const PurchaseOrderReceiptCreationPage: React.FC<IPurchaseOrderReceiptCreationPa
 
   const completePurchaseOrderReceiptCreation = () => {
     console.log('Done PO Receipt Creation');
+    history.push(`/purchase-order-receipt-record`);
   };
 
   return (
